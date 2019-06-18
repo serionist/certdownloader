060a1a21d4c12915c19982d266811a611bf2b947
Osvay Soma <osvaysoma@gmail.com>
2019-06-18 19:24:46 +0200

    initial

---
+++ Options.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace certdownloader
+{
+    public class Options
+    {
+        [Option('u', "keyvault-url", HelpText = "The URL of the KeyVault", Required = true)]
+        public string KeyVaultUrl { get; set; }
+        [Option('i', "keyvault-client-id", HelpText = "The ClientId to use", Required = true)]
+        public string KeyVaultClient { get; set; }
+        [Option('s', "keyvault-client-secret", HelpText = "The ClientSecret to use", Required = true)]
+        public string KeyVaultSecret { get; set; }
+        [Option('c', "cert-name", HelpText = "The name of the Certificate in the KeyVault", Required = true)]
+        public string CertificateName { get; set; }
+        [Option('o', "output", HelpText = "The output PFX path", Required = true, Default = "certificate.pfx")]
+        public string OutputPath { get; set; }
+        [Option('p', "cert-pass", HelpText = "The password to set to the certificate", Required = false)]
+        public string CertificatePassword { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using CommandLine;
+using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace certdownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args).WithParsed(e =>
+            {
+                try
+                {
+                    Console.WriteLine("Authenticating with KeyVault");
+                    var kv = new KeyVaultClient((string authority, string resource, string scope) =>
+                    {
+                        ClientCredential clientCredential = new ClientCredential(e.KeyVaultClient, e.KeyVaultSecret);
+                        var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+                        var result = context.AcquireTokenAsync(resource, clientCredential).Result;
+                        return Task.FromResult(result.AccessToken);
+                    });
+                    
+                   Console.WriteLine("Retrieving secret");
+                    var s = kv.GetSecretAsync(e.KeyVaultUrl, e.CertificateName).Result;
+                    Console.WriteLine("Parsing certificate");
+                    X509Certificate2Collection col = new X509Certificate2Collection();
+                    col.Import(Convert.FromBase64String(s.Value), "", X509KeyStorageFlags.Exportable);
+                    Console.WriteLine("Exporting certificate");
+                    File.WriteAllBytes(e.OutputPath, col.Export(X509ContentType.Pfx, e.CertificatePassword ?? ""));
+                   Console.WriteLine($"Success. Certificate saved to: {e.OutputPath}");
+                }
+                catch (Exception ex)
+                {
+                    var a = ex;
+                    if (a is AggregateException agg)
+                        a = agg.InnerException;
+                    Console.WriteLine($"Operation failed!\r\n{a}");
+                    Environment.Exit(1);
+                }
+            }).WithNotParsed(e =>
+            {
+                Console.WriteLine($"Argument errors! Failed to start.");
+                Environment.Exit(1);
+            });
+
+            
+        }
+    }
+}
